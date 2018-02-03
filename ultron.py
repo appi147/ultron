09b696238b023edb66c2234fe2e68c9515638f45
Arpit Choudhary <arpitkumar147@gmail.com>
2018-02-03 23:31:54 +0530

    post

--- ultron.py
+++ ultron.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+
 application = Flask(__name__)
 
+
 @application.route("/")
-def hello():
+def homepage():
     return render_template('index.html')
+
+
+@application.route("/", methods=['POST'])
+def webhook():
+    data = request.get_json()
+    return "ok"
+
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
