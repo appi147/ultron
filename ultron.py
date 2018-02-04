26690b0be5de4de34be16f30d31e972c15d8d18d
Arpit Choudhary <arpitkumar147@gmail.com>
2018-02-04 05:39:27 +0530

    add options for dialogflow and wit.ai

--- ultron.py
+++ ultron.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request
+import requests
 import apiai
 
 
-ACCESS_TOKEN = '6b19097281d4497f8126c12c9f7146a0'
+API_ACCESS_TOKEN = '6b19097281d4497f8126c12c9f7146a0'
+WIT_TOKEN = 'LNXRDEZGL53OOPT6JH7QSC7H5QJUYGNB'
+WIT_URL = 'https://api.wit.ai/message?v=04/02/2018&q='
 application = Flask(__name__)
-ai = apiai.ApiAI(ACCESS_TOKEN)
+ai = apiai.ApiAI(API_ACCESS_TOKEN)
 
 
 @application.route("/", methods=['GET'])
@@ -13,15 +16,39 @@ def homepage():
     if query is None:
         return render_template('index.html')
     else:
-        return "ok"
+        return "pass"
 
 
 def execute(query):
     pass
 
 
+def handler(query):
+    inp, resp = dialogflow(query)
+    entities = wit(query)
+    if inp is not None:
+        return 'api', resp
+    if inp is None:
+        if 'action' in entities and 'act' in entities:
+            action = entities['action'][0]['value']
+            act = entities['act'][0]['value']
+            return 'wit', action, act
+        else:
+            return 'api', resp
+    return 'fail'
+
+
+def wit(query):
+    headers = {'Authorization': 'Bearer ' + WIT_TOKEN}
+    try:
+        r = requests.get(url=WIT_URL+query, headers=headers)
+    except Exception as e:
+        print('Exception ' + e)
+        return None
+    return r.json()['entities']
+
+
 def dialogflow(query):
-    resp = []
     request = ai.text_request()
     request.lang = 'en'
     request.session_id = "pi"
@@ -30,10 +57,14 @@ def dialogflow(query):
     a = response.read()
     dt = json.loads(a)
     try:
-        resp.append(dt['result']['fulfillment']['speech'])
+        resp = dt['result']['fulfillment']['speech']
+        inp = dt['result']['action']
+        if inp == 'input.unknown':
+            inp = None
     except Exception as e:
         print('Exception ' + e)
-    return resp
+        return None, None
+    return inp, resp
 
 
 if __name__ == "__main__":
